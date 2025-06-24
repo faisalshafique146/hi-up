09ccf2f698073b0235a3d8ae7fe1a226bc7c4f00
Faisal Shafique <faisalshafique146@gmail.com>
2025-06-24 15:31:23 +0500

    feat(auth): integrate better-auth for authentication with GitHub
    - Added LoginForm component for user login with GitHub and email.
    - Implemented authentication routes using better-auth.
    - Created auth client and environment configuration for better-auth.
    - Set up Prisma database models for User, Session, Account, and Verification.
    - Integrated toast notifications for successful login and error handling.
    - Updated layout to include Toaster for notifications.
    - Added sign-out functionality and session management.
    - Updated package dependencies for better-auth and Prisma.

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,36 @@
+"use client";
+import { Button } from "@/components/ui/button";
 import { ThemeToggle } from "@/components/ui/themeToggle";
+import { authClient } from "@/lib/auth-client";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 
 export default function Home() {
+  const router = useRouter();
+  const { data: session } = authClient.useSession();
+
+  async function signOut() {
+    await authClient.signOut({
+      fetchOptions: {
+        onSuccess: () => {
+          router.push("/"); // redirect to login page
+          toast.success("You have been logged out successfully.");
+        },
+      },
+    });
+  }
   return (
     <div>
       <h1 className="text-cyan-400 text-4xl">Hello world!</h1>
       <ThemeToggle />
+      {session ? (
+        <div>
+          <p>{session.user.name}</p>
+          <Button onClick={signOut}>Logout</Button>
+        </div>
+      ) : (
+        <Button>Login</Button>
+      )}
     </div>
   );
 }
