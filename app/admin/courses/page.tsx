ee47485ed54ce7d144fae56edc9f39fb3243f083
Faisal Shafique <faisalshafique146@gmail.com>
2025-07-12 16:58:59 +0500

    feat: add AdminCourseCardSkeleton for improved loading experience in course management

--- app/admin/courses/page.tsx
+++ app/admin/courses/page.tsx
@@ -1,11 +1,14 @@
 import { adminGetCourses } from "@/app/data/admin/admin-get-courses";
 import { buttonVariants } from "@/components/ui/button";
 import Link from "next/link";
-import { AdminCourseCard } from "./_components/AdminCourseCard";
+import {
+  AdminCourseCard,
+  AdminCourseCardSkeleton,
+} from "./_components/AdminCourseCard";
 import { EmptyState } from "@/components/general/EmptyState";
+import { Suspense } from "react";
 
-export default async function CoursesPage() {
-  const data = await adminGetCourses();
+export default function CoursesPage() {
   return (
     <>
       <div className="flex items-center justify-between">
@@ -14,7 +17,17 @@ export default async function CoursesPage() {
           Create Course
         </Link>
       </div>
+      <Suspense fallback={<AdminCourseCardSkeletonLayout/>}>
+        <RenderCourses />
+      </Suspense>
+    </>
+  );
+}
 
+async function RenderCourses() {
+  const data = await adminGetCourses();
+  return (
+    <>
       {data.length === 0 ? (
         <EmptyState
           title="No Courses Found"
@@ -30,5 +43,15 @@ export default async function CoursesPage() {
         </div>
       )}
     </>
+  );
+}
+
+function AdminCourseCardSkeletonLayout() {
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-1 lg:grid-cols-2 gap-7">
+      {Array.from({ length: 4 }).map((_, index) => (
+        <AdminCourseCardSkeleton key={index} />
+      ))}
+    </div>
   );
 }
