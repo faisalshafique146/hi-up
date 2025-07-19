4a0dcf751dcef1dceaa1a6af920b95cdeb3c0ca7
Faisal Shafique <faisalshafique146@gmail.com>
2025-07-19 23:35:29 +0500

    feat: Implement course dashboard with enrolled and available courses display

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,78 @@
-export default function DashboardPage(){
-  return(
-    <h1>Hello</h1>
-  )
+import { EmptyState } from "@/components/general/EmptyState";
+import { getAllCourses } from "../data/course/get-all-courses";
+import { getEnrolledCourses } from "../data/user/get-enrolled-courses";
+import { PublicCourseCard } from "../(public)/_components/PublicCourseCard";
+import Link from "next/link";
+
+export default async function DashboardPage() {
+  const [courses, enrolledCourses] = await Promise.all([
+    getAllCourses(),
+    getEnrolledCourses(),
+  ]);
+  return (
+    <>
+      <div className="flex flex-col gap-2">
+        <h1 className="text-3xl font-bold">Enrolled Courses</h1>
+        <p className="text-muted-foreground">
+          Here you can see all the courses you have access to
+        </p>
+      </div>
+
+      {enrolledCourses.length === 0 ? (
+        <EmptyState
+          title="No Courses Purchased"
+          description="You have not Purchased any courses yet"
+          buttonText="Browse Courses"
+          href="/courses"
+        />
+      ) : (
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+          {enrolledCourses.map((course) => (
+            <Link
+              key={course.Course.id}
+              href={`/dashboard/${course.Course.slug}`}
+            >
+              {course.Course.title}
+            </Link>
+          ))}
+        </div>
+      )}
+
+      <section className="mt-10">
+        <div className="flex flex-col gap-2 mb-5">
+          <h1 className="text-3xl font-bold">Available Courses</h1>
+          <p className="text-muted-foreground">
+            Here you can see all the courses you have can purchase
+          </p>
+        </div>
+
+        {courses.filter(
+          (course) =>
+            !enrolledCourses.some(
+              ({ Course: enrolled }) => enrolled.id === course.id
+            )
+        ).length === 0 ? (
+          <EmptyState
+            title="No course available"
+            description="You have already purchases all available courses"
+            buttonText="Browse courses"
+            href="/courses"
+          />
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+            {courses
+              .filter(
+                (course) =>
+                  !enrolledCourses.some(
+                    ({ Course: enrolled }) => enrolled.id === course.id
+                  )
+              )
+              .map((course) => (
+                <PublicCourseCard key={course.id} data={course} />
+              ))}
+          </div>
+        )}
+      </section>
+    </>
+  );
 }
